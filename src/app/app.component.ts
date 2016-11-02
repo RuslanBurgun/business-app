7d6b1efc80d5f55872ffb6a8ee0bef9c5181cf9b
Ruslan Burgun <burgun90@mail.ru>
2016-11-02 21:49:47 +0300

    add get category and businesses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import { Business } from './Business';
+import { Category } from './Category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[FirebaseService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  items: FirebaseListObservable<any[]>;
+  businesses : any;
+  categories : Category[];
+ 
+   constructor(private _firebaseServcie:FirebaseService){
 
-    constructor(af: AngularFire) {
-       this.items = af.database.list('items');
-       console.log(this.items);
+   }
+
+    ngOnInit() {
+      console.log(2);
+      this._firebaseServcie.getBusinesses().subscribe(businesses=>this.businesses = businesses);
+      this._firebaseServcie.getCategories().subscribe(category=>this.categories = category );
   }
 
 }
