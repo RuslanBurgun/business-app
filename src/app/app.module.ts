c7983214df856294e022da76a54918243dceada1
Ruslan Burgun <burgun90@mail.ru>
2016-11-02 16:13:34 +0300

    first-commit install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBhkrPFbw32nj78xI5XgODU2GLRdjqzCTk',
+  authDomain: 'business-app-84d0e.firebaseapp.com',
+  databaseURL: 'https://business-app-84d0e.firebaseio.com',
+  storageBucket: 'business-app-84d0e.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +19,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
